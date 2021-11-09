7e77563edba2962a91ad0011ea8f8ec91b08fef7
CyberTrack-ng <54797508+PashaNikanovich@users.noreply.github.com>
2021-11-09 17:09:51 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+# Конфиг
+command_prefixes = ['.','!','/']
+
+ghoul_table_command = 'ghoul-c'
+
+end_message = 'ТВОЯ МАТЬ ПРЕКРАСНА'
+end_message = 'ТВОЯ МАТЬ ПРЕКРАСНА'
+end_message = 'ТВОЯ МАТЬ ПРЕКРАСНА'
+end_message = 'ТВОЯ МАТЬ ПРЕКРАСНА'
+end_message = 'ТВОЯ МАТЬ ПРЕКРАСНА'
+end_message = 'ТВОЯ МАТЬ ПРЕКРАСНА'
+end_message = 'ТВОЯ МАТЬ ПРЕКРАСНА'
+end_message = 'ТВОЯ МАТЬ ПРЕКРАСНА'
+messages_per_second = 7 # Для ghoul_spam
+sleep_time_ghoul = 0.1 # Для ghoul_table 0.0825
+
+# Линии ниже не трогать, сломается!
+
+api_id = 1016382
+api_hash = 'c27834e5683d50a9bacf835a95ec4763'
+
+# Линии выше не трогать, сломается!
